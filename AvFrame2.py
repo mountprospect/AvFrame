5fab94c66528613b32b617c8e4971b687ff16122
Maalav <maalav.pandya@gmail.com>
2023-12-19 16:31:28 -0500

    Getting basic flight info successfully

--- AvFrame2.py
+++ AvFrame2.py
@@ -8,3 +8,36 @@ ZONE = {"tl_y": 29.754081, "tl_x": -82.511874, "br_y": 29.535365, "br_x": -82.14
 
 # Time to wait in between switching info screens (seconds)
 DELAY = 3.5
+
+fr = FlightRadar24API()
+# allAirports = fr.get_airports()
+# allAirlines = fr.get_airlines()
+# allFlights = fr.get_flights()
+
+# Declare bounds using ZONE defined earlier
+bounds = fr.get_bounds(ZONE)
+
+# Find flights within zone
+flightsInZone = fr.get_flights(bounds = bounds)
+
+if (len(flightsInZone) > 0):
+    for flight in flightsInZone:
+        details = fr.get_flight_details(flight.id)
+        flight.set_flight_details(details)
+        
+        callsign = flight.callsign.upper()
+        altitude = flight.altitude
+        model = flight.aircraft_model
+        code = flight.aircraft_code
+        origin = flight.origin_airport_icao.upper()
+        destination = flight.destination_airport_icao.upper()
+        gs = flight.ground_speed
+        airline = flight.airline_name
+        status = flight.status_text
+        
+        print(callsign + "\n")
+        
+else:
+    print("No flights in zone!")
+
+
